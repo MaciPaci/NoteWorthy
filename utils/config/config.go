e12a2abfe12345293a68860814f5809ff36ef072
maciej.stachowiak <maciej.stachowiak@olx.pl>
2023-01-22 01:47:08 +0100

    added struct docs

--- utils/config/config.go
+++ utils/config/config.go
@@ -5,10 +5,12 @@ import (
 	"os"
 )
 
+// Config is a container for configuration variables
 type Config struct {
 	Token string `json:"bot_token"`
 }
 
+// LoadConfig loads configuration from JSON file into Conf struct
 func LoadConfig(filepath string) (*Config, error) {
 	data, err := os.ReadFile(filepath)
 	if err != nil {
